457ffe06c3bdf338b5944836cfb7b51b2c94ac9e
Michael Tal <mi-tal.dev@pm.me>
2026-07-27 10:42:35 +0200

    image: overwrite os-release fields with custom values

--- build/00-image-info.sh
+++ build/00-image-info.sh
@@ -64,8 +64,7 @@ echo "  image-vendor: ${IMAGE_VENDOR}"
 ###############################################################################
 # Customize /usr/lib/os-release
 ###############################################################################
-# Only modify if the file exists and VARIANT_ID is not already set
-if [[ -f "${OS_RELEASE}" ]] && ! grep -q "^VARIANT_ID=" "${OS_RELEASE}"; then
+if [[ -f "${OS_RELEASE}" ]]; then
     # Read existing values
     if [[ -n "${VERSION:-}" ]]; then
         OS_VERSION="${VERSION}"
@@ -73,21 +72,48 @@ if [[ -f "${OS_RELEASE}" ]] && ! grep -q "^VARIANT_ID=" "${OS_RELEASE}"; then
         OS_VERSION="${UBLUE_IMAGE_TAG}"
     fi
 
-    # Append our identity
-    cat >>"${OS_RELEASE}" <<EOF
+    CUSTOM_VERSION="${OS_VERSION} (${IMAGE_PRETTY_NAME})"
 
-# ${IMAGE_NAME} image identity
-VARIANT_ID="${IMAGE_FLAVOR}"
-PRETTY_NAME="${IMAGE_PRETTY_NAME}"
-NAME="${IMAGE_NAME}"
-IMAGE_ID="${IMAGE_NAME}"
-IMAGE_VERSION="${OS_VERSION}"
-ID_LIKE="${IMAGE_LIKE}"
-HOME_URL="${HOME_URL}"
-DOCUMENTATION_URL="${DOCUMENTATION_URL}"
-SUPPORT_URL="${SUPPORT_URL}"
-BUG_REPORT_URL="${BUG_REPORT_URL}"
-EOF
+    TMP_FILE=$(mktemp)
+    # Update existing keys or add new ones
+    awk -v flavor="${IMAGE_FLAVOR}" \
+        -v pretty_name="${IMAGE_PRETTY_NAME}" \
+        -v name="${IMAGE_NAME}" \
+        -v image_id="${IMAGE_NAME}" \
+        -v image_version="${OS_VERSION}" \
+        -v id_like="${IMAGE_LIKE}" \
+        -v home_url="${HOME_URL}" \
+        -v doc_url="${DOCUMENTATION_URL}" \
+        -v support_url="${SUPPORT_URL}" \
+        -v bug_url="${BUG_REPORT_URL}" \
+        -v custom_version="${CUSTOM_VERSION}" '
+    {
+        # Update existing keys
+        if ($0 ~ /^VARIANT_ID=/) { print "VARIANT_ID=\"" flavor "\""; next }
+        if ($0 ~ /^PRETTY_NAME=/) { print "PRETTY_NAME=\"" pretty_name "\""; next }
+        if ($0 ~ /^NAME=/) { print "NAME=\"" name "\""; next }
+        if ($0 ~ /^VERSION=/) { print "VERSION=\"" custom_version "\""; next }
+        if ($0 ~ /^ID_LIKE=/) { print "ID_LIKE=\"" id_like "\""; next }
+        if ($0 ~ /^HOME_URL=/) { print "HOME_URL=\"" home_url "\""; next }
+        if ($0 ~ /^DOCUMENTATION_URL=/) { print "DOCUMENTATION_URL=\"" doc_url "\""; next }
+        if ($0 ~ /^SUPPORT_URL=/) { print "SUPPORT_URL=\"" support_url "\""; next }
+        if ($0 ~ /^BUG_REPORT_URL=/) { print "BUG_REPORT_URL=\"" bug_url "\""; next }
+
+        # Print all other lines as-is
+        print $0
+    }
+    ' "${OS_RELEASE}" > "${TMP_FILE}"
+
+    # Add new keys if they don't exist
+    if ! grep -q "^IMAGE_ID=" "${TMP_FILE}"; then
+        echo "IMAGE_ID=\"${IMAGE_NAME}\"" >> "${TMP_FILE}"
+    fi
+    if ! grep -q "^IMAGE_VERSION=" "${TMP_FILE}"; then
+        echo "IMAGE_VERSION=\"${OS_VERSION}\"" >> "${TMP_FILE}"
+    fi
+
+    mv "${TMP_FILE}" "${OS_RELEASE}"
 
     echo "Customized ${OS_RELEASE}"
+    echo "  VERSION: ${CUSTOM_VERSION}"
 fi
